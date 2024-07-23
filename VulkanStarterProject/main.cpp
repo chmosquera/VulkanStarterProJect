dd2910daa411c970b01161580ff3090123669029
Chanelle Mosquera <166346498+chanelle-stemtales@users.noreply.github.com>
2024-07-23 15:59:54 -0700

    Vulkan setup instance

--- VulkanStarterProject/main.cpp
+++ VulkanStarterProject/main.cpp
@@ -1,14 +1,118 @@
-//
-//  main.cpp
-//  VulkanStarterProject
-//
-//  Created by Chanelle Mosquera on 7/23/24.
-//
+#define GLFW_include_vulkan
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    
+    GLFWwindow * window;
+    VkInstance instance;
+    
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Welcome to Vulkan", nullptr, nullptr);
+        
+        
+    }
+    
+    void initVulkan() {
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (glfwWindowShouldClose(window) == false) {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    
+    void createInstance() {
+        
+        // Optional. Provides data to the driver to optimize our application.
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 1);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        // Required info about the instance.
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        // Extensions are necessary to interface the hardware API to window system
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+                
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        // Required for MacOS to avoid VK_ERROR_INCOMPATIBLE_DRIVER
+        std::vector<const char*> requiredExtensions;
+        for (uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        // ---
+        
+        // Provide data about extensions to user
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        std::cout << "Available extensions:\n";
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        
+        createInfo.enabledLayerCount = 0;
+        
+        // Create Vulkan instance
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if ( result != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create instance.");
+        }
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
